8b1274065822703b0e7fc4c567a2a03244e1cf98
Jeffrowetull <jeffrowetull@gmail.com>
2020-02-10 13:59:08 -0800

    Update 11_args.py

--- src/11_args.py
+++ src/11_args.py
@@ -5,7 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
+def f1(x,y):
+    return x+y
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
@@ -13,7 +14,11 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
-
+def f2(*args):
+  if type(args[0]) == list:
+    return sum(args[0])
+  else:
+    return sum(args
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -30,7 +35,11 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-
+def f3(x,y=0):
+    if y ==0:
+        return x+1
+    else:
+        return x+y
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -44,7 +53,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print(f'key: {key}, value: {value}')
 # Should print
 # key: a, value: 12
 # key: b, value: 30
